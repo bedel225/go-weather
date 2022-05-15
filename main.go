20d26ea91c924b7e3a1662306ec6e73ad1116975
bedel225 <abdoulchoco@yahoo.fr>
2022-05-15 10:00:00 +0200

    Initial commit - Go weather API

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Weather struct {
+	Name string `json:"name"`
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
+}
 
 func main() {
-	fmt.Println("Hello from Go 1.16")
+	apiKey := "ta_clef_api_openweather" // à remplacer
+	city := "Paris"
+
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric", city, apiKey)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	var w Weather
+	if err := json.NewDecoder(resp.Body).Decode(&w); err != nil {
+		fmt.Println("Erreur de décodage :", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("La température à %s est %.1f°C\n", w.Name, w.Main.Temp)
 }
